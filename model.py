e06b2c50eee468738b5666a79549e4dcab882221
poriver <92435559+poriver@users.noreply.github.com>
2021-12-27 16:46:16 +0800

    Add files via upload

--- model.py
+++ model.py
@@ -7,9 +7,10 @@ import numpy as np
 from torch.nn import functional as F
 from my_resnet2 import RestNet18
 from my_resnet import resnet_test
+from test_model import Psz
 
 
-model = resnet_test()
+model = RestNet18()
 # print(model)
 model.to(lib.device)
 
@@ -19,14 +20,15 @@ model.to(lib.device)
 # output = model(x)
 # print(output.shape)
 
-# loss_fn = F.nll_loss()
-# loss_fn.to(lib.device)
+loss_fn = nn.CrossEntropyLoss()
+loss_fn.to(lib.device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=lib.learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=lib.learning_rate)
 
 if os.path.exists("./my_model.pkl"):
     model.load_state_dict(torch.load("./my_model.pkl"))
     optimizer.load_state_dict(torch.load("./optimizer.pkl"))
+    print("使用已有模型进行训练")
 
 def train(epoch):
     print("使用{}训练".format(lib.device))
@@ -41,13 +43,28 @@ def train(epoch):
         # 梯度归零
         optimizer.zero_grad()
         output = model(input)
-        loss = F.nll_loss(output, target)
+        loss = loss_fn(output, target)
         loss.backward()
+        # 输出权重
+        for name, parms in model.named_parameters():
+            print('-->name:', name, '-->grad_requirs:', parms.requires_grad, ' -->grad_value:', parms.grad)
+
         optimizer.step()
 
         print(epoch, idx, loss.item())
 
-        if idx % 100 == 0:
+        if idx>0 and idx % 100 == 0:
+
+            # 写入实验数据
+            file = open("transform_5.txt", "r")
+            batch = file.readlines()[-1].split(",")[0].split(":")[-1]
+            batch = int(batch) + 100
+            # print(batch)
+
+            file = open("transform_5.txt", "a")
+            file.write("\n"+"batch:{},loss:{}".format(batch, loss.item()))
+            file.close()
+
             # print("训练次数:{}，Loss:{}".format(idx, loss.item()))
             torch.save(model.state_dict(), "./my_model.pkl")
             torch.save(optimizer.state_dict(), "./optimizer.pkl")
@@ -75,9 +92,9 @@ def eval():
     # print("模型已保存")
 
 if __name__ == '__main__':
-    # for i in range(1):
-    #     train(i)
-    eval()
+    for i in range(1):
+        train(i)
+    # eval()
 
 
 
